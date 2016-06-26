ddc71dc23e1988c56195decf9c22a95b055caad0
Joseph Spurrier <github@josephspurrier.com>
2016-06-26 14:09:15 -0400

    Add Bower and update gulp
    Closes #6

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 
-// Style Task
-gulp.task('style', function() {
+// SASS Task
+gulp.task('sass', function() {
     var sass = require('gulp-sass');
     gulp.src('asset/dynamic/sass/**/*.scss')
         // Available for outputStyle: expanded, nested, compact, compressed
@@ -9,10 +9,34 @@ gulp.task('style', function() {
         .pipe(gulp.dest('asset/static/css/'));
 });
 
+// jQuery Task
+gulp.task('jquery', function() {
+	gulp.src('bower_components/jquery/dist/jquery.min.*')
+		.pipe(gulp.dest('asset/static/js/'));
+});
+
+// Bootstrap Task
+gulp.task('bootstrap', function() {
+	gulp.src('bower_components/bootstrap/dist/css/bootstrap-theme.min.*')
+		.pipe(gulp.dest('asset/static/css/'));
+	gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.*')
+		.pipe(gulp.dest('asset/static/css/'));
+	gulp.src('bower_components/bootstrap/dist/fonts/*')
+		.pipe(gulp.dest('asset/static/fonts/'));
+	gulp.src('bower_components/bootstrap/dist/js/bootstrap.min.js')
+		.pipe(gulp.dest('asset/static/js/'));
+});
+
+// Underscore Task
+gulp.task('underscore', function() {
+	gulp.src('bower_components/underscore/underscore-min.*')
+		.pipe(gulp.dest('asset/static/js/'));
+});
+
 // Watch
 gulp.task('watch', function() {
-    gulp.watch('asset/dynamic/sass/**/*.scss', ['style']);
+    gulp.watch('asset/dynamic/sass/**/*.scss', ['sass']);
 });
 
 // Default
-gulp.task('default', ['style']);
+gulp.task('default', ['sass', 'jquery', 'bootstrap', 'underscore']);
