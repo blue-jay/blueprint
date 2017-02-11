9e33d14e79a610e4135c675542e74ec30fd4a957
Joseph Spurrier <github@josephspurrier.com>
2017-02-11 16:18:48 -0500

    Add files to support autoreloading.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 // Modules
-var gulp = require('gulp');
-var favicon = require ('gulp-real-favicon');
-var fs = require('fs');
+var gulp        = require('gulp');
+var favicon     = require('gulp-real-favicon');
+var fs          = require('fs');
 var runSequence = require('run-sequence');	// Using until gulp v4 is released
+var reload      = require('gulp-livereload');
+var sync        = require('gulp-sync')(gulp).sync;
+var child       = require('child_process');
+var util        = require('gulp-util');
 
 // Enviroment variables
 var env = JSON.parse(fs.readFileSync('./env.json'))
@@ -11,6 +15,9 @@ var folderView = env.View.Folder;
 
 // Other variables
 var faviconData = folderAsset + '/dynamic/favicon/data.json';
+
+// Application server
+var server = null;
 
 // SASS Task
 gulp.task('sass', function() {
@@ -24,7 +31,8 @@ gulp.task('sass', function() {
 		// Available for outputStyle: expanded, nested, compact, compressed
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(ext('.min.css'))
-		.pipe(gulp.dest(folderAsset + '/static/css/'));
+		.pipe(gulp.dest(folderAsset + '/static/css/'))
+		.pipe(reload());
 });
 
 // JavaScript Task
@@ -39,7 +47,8 @@ gulp.task('javascript', function() {
 				min:'.min.js'
 			}
 		}))
-		.pipe(gulp.dest(folderAsset + '/static/js/'));
+		.pipe(gulp.dest(folderAsset + '/static/js/'))
+		.pipe(reload());
 });
 
 // jQuery Task
@@ -145,14 +154,88 @@ gulp.task('favicon-update', function(done) {
 	});
 });
 
-// Watch
-gulp.task('watch', function() {
+// Monitor Go files for changes
+gulp.task('server:watch', function() {
+	// Restart application
+	gulp.watch([
+		'*/**/*.tmpl',
+		'env.json'
+	], ['server:spawn']);
+	
+	// Rebuild and restart application server
+	gulp.watch([
+		'*.go',
+		'*/**/*.go'
+	], [
+		'server:build',
+		'server:spawn'
+	]);
+});
+
+// Build application from source
+gulp.task('server:build', function() {
+	var build = child.spawnSync('go', ['build']);
+	if (build.stderr.length) {
+		var lines = build.stderr.toString()
+		.split('\n').filter(function(line) {
+		return line.length
+		});
+		for (var l in lines)
+			util.log(util.colors.red(
+			'Error (go build): ' + lines[l]
+		));
+		notifier.notify({
+			title: 'Error (go build)',
+			message: lines
+		});
+	}
+	return build;
+});
+
+// Spawn an application process
+gulp.task('server:spawn', function() {
+	if (server)
+		server.kill();
+	
+	// Spawn application server
+	server = child.spawn('blueprint.exe');
+	
+	// Trigger reload upon server start
+	server.stdout.once('data', function() {
+		reload.reload('/');
+	});
+	
+	// Pretty print server log output
+	server.stdout.on('data', function(data) {
+		var lines = data.toString().split('\n')
+		for (var l in lines)
+		if (lines[l].length)
+		util.log(lines[l]);
+	});
+	
+	// Print errors to stdout
+	server.stderr.on('data', function(data) {
+		process.stdout.write(data.toString());
+	});
+});
+
+// Main watch function.
+gulp.task('watch', ['server:build'], function() {
+	// Start the listener (use with the LiveReload Chrome Extension)
+	reload.listen();
+
+	// Watch the assets
 	gulp.watch(folderAsset + '/dynamic/sass/**/*.scss', ['sass']);
 	gulp.watch(folderAsset + '/dynamic/js/*.js', ['javascript']);
+	
+	return gulp.start(sync([
+		'server:watch',
+		'server:spawn'
+	]));
 });
 
 // Init - every task
-gulp.task('init', ['sass', 'javascript', 'jquery', 'bootstrap', 'underscore', 'favicon']);
+gulp.task('init', ['sass', 'javascript', 'jquery', 'bootstrap', 'underscore', 'favicon', 'server:build']);
 
 // Default - only run the tasks that change often
-gulp.task('default', ['sass', 'javascript']);
+gulp.task('default', ['sass', 'javascript', 'server:build']);
